8ca3ad67c54a2e706bbed1f320220970291dcb58
rezashabrang <53796036+rezashabrang@users.noreply.github.com>
2020-11-27 02:28:15 +0330

    Processing The input

--- subinterval.cpp
+++ subinterval.cpp
@@ -8,11 +8,13 @@ int main() {
     // n
     int n;
     cin >> n;
-    int array_numbers[n];
+    int* array_numbers = new int[n];
     // Getting Numbers
     for (int i = 0; i < n; i++) {
         cin >> array_numbers[i];
     }
-    cout << array_numbers;
+    for (int i = 0; i < n; i++) {
+        cout << array_numbers[i]<<endl;
+    }
     return 0;
 }
