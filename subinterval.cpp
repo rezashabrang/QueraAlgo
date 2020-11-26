8b2776e4e86db45cc8f14da5d9d4f0f54de9d451
rezashabrang <53796036+rezashabrang@users.noreply.github.com>
2020-11-27 02:24:03 +0330

    initial commit

--- subinterval.cpp
+++ subinterval.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cstring>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // n
+    int n;
+    cin >> n;
+    int array_numbers[n];
+    // Getting Numbers
+    for (int i = 0; i < n; i++) {
+        cin >> array_numbers[i];
+    }
+    cout << array_numbers;
     return 0;
 }
